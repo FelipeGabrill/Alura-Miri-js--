f8551409d0b9a06e9efc54f47fa96228f27c455d
Felipe Gabriel <felipegabrielodss@gmail.com>
2024-07-30 02:06:54 -0300

    Eventos e lógicas

--- main.js
+++ main.js
@@ -1,5 +1,12 @@
-function tocaSom (idElementoAudio) {
-    document.querySelector(idElementoAudio).play();
+function tocaSom (seletorAudio) {
+
+    const elemento = document.querySelector(seletorAudio);
+
+    if (elemento || elemento.localName === 'audio') {
+        elemento.play();
+    } else {
+        alert('Elemento não encontrado ou seletor inválido!');
+    }
 }
 
 const listaDeTeclas = document.querySelectorAll('.tecla');
@@ -13,4 +20,13 @@ for(let i = 0; i < listaDeTeclas.length; i++){
     tecla.onclick = function() {
         tocaSom(idAudio);
     };
+
+    tecla.onkeydown = function(evento) {
+        if (evento.code === 'Space' || evento.code === 'Enter')
+            tecla.classList.add('ativa');
+    }
+    
+    tecla.onkeyup = function() {
+        tecla.classList.remove('ativa');
+    }
 }
